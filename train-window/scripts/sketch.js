8b4657004d759f7c0baac9426ec4890a0fd6f280
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-12 15:03:26 +0000

    feat(train-window/index.html-&&-train-window/scripts/sketch.js-&&-train-window/style.css): style webpage

--- train-window/scripts/sketch.js
+++ train-window/scripts/sketch.js
@@ -5,34 +5,37 @@ let trainWindow;
 let trainMovement = 0;
 
 function setup() {
-  createCanvas(500, 500);
+  let canvas = createCanvas(500, 500);
+
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
 
   // frameRate() is lowered to 24fps for better stability. 24fps is also a common frame rate in animation
   frameRate(24);
-  
+
   trainWindow = {
     w: 2 * width / 3,
     h: 2 * height / 3,
     x: width / 6,
     y: height / 6
   }
-    
+
   noStroke();
-  
+
   // Initialise the mountains. The mountain canvas is slightly larger than the rounded rectangle frame so it has some leeway to bounce
   setupMountains(5.5 * width / 9, 5.5 * height / 9);
 }
 
 function draw() {
   background(color("#83310c"));
-  
+
   // Bounce uses perlin noise to move the window slightly on the y-axis, giving the illusion of the train rumbling
   let bounce = noise(trainMovement) * 10;
-  
+
   // Draw the train window
   fill(color("#4f1d07"));
   rect(trainWindow.x, trainWindow.y + bounce, trainWindow.w, trainWindow.h);
-  
+
   // Mountains are drawn clipped to a rounded rect
   push();
   beginClip();
@@ -42,7 +45,7 @@ function draw() {
   renderMountains();
   pop();
 
-  
+
   // Create the blinds
   fill(color("#E4B7A4"));
   stroke(color("#5D2913"));
@@ -51,6 +54,6 @@ function draw() {
   rect(trainWindow.x, trainWindow.y + bounce, trainWindow.w, blindHeight);
   rect(trainWindow.x, trainWindow.y + blindHeight + bounce, trainWindow.w, blindHeight);
   rect(trainWindow.x, trainWindow.y + blindHeight * 2 + bounce, trainWindow.w, blindHeight / 15);
-  
+
   trainMovement += 0.05;
 }
